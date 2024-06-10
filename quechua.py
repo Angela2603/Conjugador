bbf811227c91bfadd826e4d6a6bcddd4598a8d00
Angela2603 <a20180405@pucp.edu.pe>
2024-06-10 16:21:20 -0500

    intento tema

--- quechua.py
+++ quechua.py
@@ -1,3 +1,9 @@
+## tema
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
 ## importamos streamlit
 import streamlit as st
 
